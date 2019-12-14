d7133d4973411dd9ee7798bc4474edf2cec557bc
Kutman Mairambek uulu <muctep.k@gmail.com>
2019-12-14 12:57:13 +0600

    Выводится весь список пользователей

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
-# Register your models here.
+from webapp.models import Photo, Comment, LikeSystem
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(LikeSystem)
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from webapp.models import Photo
 
@@ -7,7 +7,22 @@ from webapp.models import Photo
 class PhotoListView(ListView):
     template_name = 'index.html'
     model = Photo
+    extra_context = {"title": "Фото"}
+    context_object_name = 'photos'
 
 class PhotoCreateView(CreateView):
     template_name = 'create.html'
+    model = Photo
+    extra_context = {"title": "Фото"}
+
+
+class PhotoUpdateView(UpdateView):
+    template_name = 'update.html'
+    model = Photo
+    extra_context = {"title": "Фото"}
+
+
+class PhotoDeleteView(DeleteView):
+    template_name = 'delete.html'
+    model = Photo
 
