b4d598ac5aec03aa203d9b1e4841d4654d7c57fe
Kutman Mairambek uulu <muctep.k@gmail.com>
2019-12-14 13:16:40 +0600

    Работает удаление и изменение фото

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from webapp.views import PhotoListView, PhotoCreateView
+from webapp.views import PhotoListView, PhotoCreateView, PhotoUpdateView, PhotoDeleteView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('', PhotoListView.as_view(), name='index'),
-    path('create/', PhotoCreateView.as_view(), name='create_photo')
+    path('create/', PhotoCreateView.as_view(), name='create_photo'),
+    path('photo/<int:pk>/update/', PhotoUpdateView.as_view(), name='update_photo'),
+    path('photo/<int:pk>/delete/', PhotoDeleteView.as_view(), name='delete_photo')
 ]
 app_name = 'webapp'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 from webapp.forms import PhotoForm
 from webapp.models import Photo
@@ -14,13 +15,15 @@ class PhotoListView(ListView):
     def get_queryset(self):
         return Photo.objects.all().order_by('-created_at')
 
+class PhotoDetailView(DetailView):
+    template_name = 'photo.html'
 
 class PhotoCreateView(CreateView):
     form_class = PhotoForm
     template_name = 'create.html'
     model = Photo
     extra_context = {"title": "Фото"}
-
+    success_url = reverse_lazy('webapp:index')
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.author = self.request.user
@@ -28,12 +31,20 @@ class PhotoCreateView(CreateView):
 
 
 class PhotoUpdateView(UpdateView):
+    form_class = PhotoForm
     template_name = 'update.html'
     model = Photo
     extra_context = {"title": "Фото"}
+    success_url = reverse_lazy('webapp:index')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        return super().form_valid(form)
 
 
 class PhotoDeleteView(DeleteView):
     template_name = 'delete.html'
     model = Photo
+    success_url = reverse_lazy('webapp:index')
 
