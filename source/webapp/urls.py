93515906618cf57fa666b63f86b66e47c9d0151f
Kutman Mairambek uulu <muctep.k@gmail.com>
2019-12-14 13:09:42 +0600

    Работает создание фото

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from webapp.views import PhotoListView
+from webapp.views import PhotoListView, PhotoCreateView
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('', PhotoListView.as_view(), name='index')
+    path('', PhotoListView.as_view(), name='index'),
+    path('create/', PhotoCreateView.as_view(), name='create_photo')
 ]
 app_name = 'webapp'
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
+from webapp.forms import PhotoForm
 from webapp.models import Photo
 
 
@@ -10,10 +11,20 @@ class PhotoListView(ListView):
     extra_context = {"title": "Фото"}
     context_object_name = 'photos'
 
+    def get_queryset(self):
+        return Photo.objects.all().order_by('-created_at')
+
+
 class PhotoCreateView(CreateView):
+    form_class = PhotoForm
     template_name = 'create.html'
     model = Photo
     extra_context = {"title": "Фото"}
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.author = self.request.user
+        return super().form_valid(form)
 
 
 class PhotoUpdateView(UpdateView):
